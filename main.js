33951c9934a3548405d175bdc9340c8f68bbaab5
ycanas <yairca3.7@gmail.com>
2023-06-09 10:09:47 -0500

    formateo de texto y nuevos estilos de salida de texto

--- main.js
+++ main.js
@@ -6,42 +6,42 @@ function calculateImc(){
 
     (!height.checkValidity() || !weight.checkValidity()) ? valid.classList.add("was-validated") : valid.classList.remove("was-validated");
     
-    let imc = weight.valueAsNumber/Math.pow(height.valueAsNumber, 2);
+    let imc = weight.valueAsNumber / Math.pow(height.valueAsNumber, 2);
     
-    let output = "Resultado: " + imc.toFixed(1);
+    let output = "Resultado 👉 " + imc.toFixed(1);
     let alert  = "container w-75 alert";
 
-    if(imc <= 18.5){
-        output += " Peso Bajo.";
+    if (imc <= 18.5) {
+        output += ", peso bajo 😟.";
         alert  += " alert-info";
     }
 
-    else if(imc <= 25){
-        output += " Peso Normal.";
+    else if (imc <= 25) {
+        output += ", peso normal 😎.";
         alert  += " alert-success";
     }
 
-    else if(imc <= 30){
-        output += " Sobrepeso";
+    else if (imc <= 30) {
+        output += ", sobrepeso 😦";
         alert  += " alert-warning";
     }
 
-    else if(imc <= 35){
-        output += " Obesidad Leve.";
+    else if (imc <= 35) {
+        output += ", obesidad leve 😧.";
         alert  += " alert-danger";
     }
 
-    else if(imc <= 40){
-        output += " Obesidad Media.";
+    else if (imc <= 40) {
+        output += ", obesidad media 😨.";
         alert  += " alert-danger";
     }
 
-    else{
-        output += " Obesidad Mórbida.";
+    else {
+        output += ", obesidad mórbida 😱.";
         alert  += " alert-danger";
     }
 
-    if(!isNaN(imc)){
+    if (!isNaN(imc)) {
         result.style.display = "";
         result.className = alert;
         result.innerHTML = `${output}`;
@@ -50,7 +50,7 @@ function calculateImc(){
         weight.value = "";
     }
 
-    if(isNaN(imc)){
+    if (isNaN(imc)) {
         result.style.display = "none";
     }
 }
